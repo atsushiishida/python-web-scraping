65b1bb03b23a9b4d876fb6d463c2e51d515b8807
Atsushi Ishida <dolin@Atsushi-no-MacBook-Air.local>
2018-10-31 10:01:48 +0900

    scrapy

---
+++ codes/scrapy/spiders/complex.py
@@ -0,0 +1,35 @@
+import scrapy
+
+
+class ComplexSpider(scrapy.Spider):
+    name = 'complex'
+    allowed_domains = ['docs.pyq.jp']
+    start_urls = ['https://docs.pyq.jp/_static/assets/scraping/item-list.html']
+
+    def parse(self, response):
+        # 商品情報を格納するリスト
+        items = []
+
+        div_item_list = response.css('div.item-list')[0]
+        for div_item in div_item_list.css('div.item'):
+            # 商品名の取得
+            item_name = div_item.css('a.item-name::text').extract_first()
+            item_name = item_name.strip()
+            
+            
+            # 商品リンクURLの取得
+            item_link = div_item.css('a.item-name::attr(href)').extract_first()
+            item_link = item_link.strip()
+            
+            
+            item_info = {
+                'name': item_name,
+                'link': item_link,
+            }
+
+            # 商品情報の辞書をリストに追加
+            items.append(item_info)
+            
+        
+        return items
+
---
+++ codes/scrapy/spiders/follow_links.py
@@ -0,0 +1,52 @@
+import scrapy
+
+
+class FollowLinksSpider(scrapy.Spider):
+    name = 'follow_links'
+    allowed_domains = ['docs.pyq.jp']
+    start_urls = [
+        'https://docs.pyq.jp/_static/assets/scraping/item-list.html'
+    ]
+
+    def parse(self, response):
+        div_item_list = response.css('div,item-list')[0]
+        for div_item in div_item_list.css('div.item'):
+            # 商品URLの取得
+            item_url = div_item.css('a.item-name::attr(href)').extract_first()
+
+            # 商品URLを個別にスクレイピング
+            yield scrapy.Request(item_url, callback=self.parse_item_page)
+
+    def parse_item_page(self, response):
+        # 商品ページのHTML解析
+
+        # 商品URLの取得
+        item_url = response.url
+        # 商品名の取得
+        item_name = response.css('h1.item-name::text').extract_first()
+        item_name = item_name.strip()
+
+        # メーカーの取得
+        item_maker = response.css('span.item-maker::text').extract_first()
+        item_maker = item_maker.strip()
+        # 価格の取得
+        item_price = response.css('span.item-price::text').extract_first()
+        item_price = item_price.strip()
+        # 在庫の取得
+        item_stock = response.css('span.item-stock::text').extract_first()
+        item_stock = item_stock.strip()
+        # 発売日の取得
+        item_release_date = response.css('span.item-release-date::text').extract_first()
+        item_release_date = item_release_date.strip()
+        # 解析した内容を辞書にする
+        item_info = {
+            'name': item_name,
+            'maker': item_maker,
+            'price': item_price,
+            'stock': item_stock,
+            'release_date': item_release_date,
+            'url': item_url,
+        }
+        
+        return item_info
+
